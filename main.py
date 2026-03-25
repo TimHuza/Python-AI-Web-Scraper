586e43621f95f3993ba4f60c237561910e74a44f
Tim <tim.ca2023@gmail.com>
2026-03-24 21:03:51 -0400

    cleaning dom content

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # "Selenium" allows us to automate a web browser so we can actually navigate to a webpage
-from scrape import scrape_website
+from scrape import scrape_website, extra_body_content, split_dom_content, clean_body_content
 import streamlit as st
 
 st.title("AI Web Scraper")
@@ -7,5 +7,12 @@ url = st.text_input("Enter a Website URL: ")
 
 if st.button("Scrape Site"):
     st.write("Scraping the website")
+
     result = scrape_website(url)
-    print(result)
+    body_content = extra_body_content(result)
+    clean_content = clean_body_content(body_content)
+
+    st.session_state_dom_scrape = clean_content
+
+    with st.expander("View DOM Content"):
+        st.text_area("DOM Content", clean_content, height=300)
