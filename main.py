bf8ba51b2a1a85ac102cf7d25b1f17f8fcb7a528
Tim <tim.ca2023@gmail.com>
2026-03-25 21:05:19 -0400

    parsing the content with Ollama

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # "Selenium" allows us to automate a web browser so we can actually navigate to a webpage
 from scrape import scrape_website, extra_body_content, split_dom_content, clean_body_content
+from parse import parse_with_ollama
 import streamlit as st
 
 st.title("AI Web Scraper")
@@ -16,3 +17,14 @@ if st.button("Scrape Site"):
 
     with st.expander("View DOM Content"):
         st.text_area("DOM Content", clean_content, height=300)
+
+if "dom_content" in st.session_state:
+    parse_description = st.text_area("Describe what you want to parse?")
+
+    if st.button("Parse Content"):
+        if parse_description:
+            st.write("Parsing the content")
+
+            dom_chunks = split_dom_content(st.session_state.dom_content)
+            result = parse_with_ollama(dom_chunks, parse_description)
+            st.write(result)
